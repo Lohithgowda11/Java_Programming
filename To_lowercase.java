bc4444c70f1ee0f431dc7f5b432ed3309b5adf9c
LOHITH GOWDA C V <122782406+Lohithgowda11@users.noreply.github.com>
2023-02-16 20:47:10 +0530

    Add files via upload
    Converting from upper case to lower case

---
+++ To_lowercase.java
@@ -0,0 +1,24 @@
+package programming;
+
+import java.util.Scanner;
+
+public class To_lowercase 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String in upper case..........");
+		String str = sc.nextLine();
+		String ans="";
+		for (int i = 0; i < str.length(); i++)
+		{
+			char ch =str.charAt(i);
+			int x=(int)ch;
+			ans=ans+(char)(x-32);
+			
+		}
+		System.out.println("To upper case......"+ans);
+		
+	}
+
+}
