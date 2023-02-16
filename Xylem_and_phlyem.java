944e034fff235080b7851cd72a71a7951821c011
LOHITH GOWDA C V <122782406+Lohithgowda11@users.noreply.github.com>
2023-02-16 20:33:57 +0530

    Add files via upload
    Program to find the number is Xylem or Phloem number

--- Xylem_and_phlyem.java
+++ Xylem_and_phlyem.java
@@ -2,27 +2,25 @@ package programming;
 
 import java.util.Scanner;
 
-public class Xylem_and_phlyem 
-{
-	public static void main(String[] args) 
-	{
+public class Xylem_and_phlyem {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num= sc.nextInt();
-		int copy=num;
-		int isum=0;
-		int osum=0;
-		while(num!=0)
-		{
-			if(num==copy ||num<10)
-				osum=osum+num%10;
+		System.out.println("enter the number");
+		int num = sc.nextInt();
+		int copy = num;
+		int isum = 0;
+		int osum = 0;
+		while (num != 0) {
+			if (num == copy || num < 10)
+				osum = osum + num % 10;
 			else
-				isum=isum+num%10;
-			num=num%10;
+				isum = isum + num % 10;
+			num=num/10;
 		}
-		if(isum==osum)
-			System.out.println(copy+" it is a xylem");
+		if (isum == osum)
+			System.out.println(copy + " it is a xylem");
 		else
-			System.out.println(copy+" it is a Phloyem");
-		
+			System.out.println(copy + " it is a Phloyem");
+
 	}
 }
